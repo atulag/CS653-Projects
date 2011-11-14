3b197dcbca98ddbf0e47a2143c987c6f59dff579
Atul Agarwal <atulagarwal104@gmail.com>
2011-11-14 23:10:49 +0530

    Helper function getStringFromTransforms is implemented

--- SVGOps.hs
+++ SVGOps.hs
@@ -12,22 +12,24 @@ getSVGStringFromElement::Element->SVG String
 
 getSVGStringFromElement (Line (PointC (x1, y1), PointC (x2, y2)) scolor swidth, transList) =
     SVGC $ concat ["<line x1=\"", show x1, "\" y1=\"", show y1, "\" x2=\"", show x2,
-                   "\" y2=\"", show y2, "\" style=\"", (strokeColor scolor), (strokeWidth swidth), "\"/>", "\n"]
+                   "\" y2=\"", show y2, "\" style=\"", (strokeColor scolor), (strokeWidth swidth), "\"",
+                    getStringFromTransforms transList, "/>", "\n"]
 
 getSVGStringFromElement (Rect (PointC (x,y)) (DimensionC width, DimensionC height) fcolor scolor swidth, transList) =
     SVGC $ concat ["<rect x=\"", show x, "\" y =\"", show y,"\" width =\"", show width,"\" height =\"", show height,
-                   "\" style =\"",(fillColor fcolor), (strokeColor scolor), (strokeWidth swidth), "\"/>", "\n"]
+                   "\" style =\"",(fillColor fcolor), (strokeColor scolor), (strokeWidth swidth), "\"",
+                   getStringFromTransforms transList, "/>", "\n"]
 
 
 
 
 getSVGStringFromElement ((Polyline points scolor swidth),transList) = 
     SVGC $ concat ["<polyline fill=\"none\" style =\"",(strokeColor scolor), (strokeWidth swidth),
-                   "\" points = \"",(printPointList points),"\"/>","\n"]
+                   "\" points = \"",(printPointList points),"\"", getStringFromTransforms transList, "/>","\n"]
 
 getSVGStringFromElement ((Polygon points fcolor scolor swidth), transList) = 
     SVGC $ concat ["<polygon style =\"", (fillColor fcolor), (strokeColor scolor), (strokeWidth swidth),
-                   "\" points = \"", (printPointList points),"\"/>","\n"]
+                   "\" points = \"", (printPointList points),"\"",getStringFromTransforms transList,"/>","\n"]
 
 
 getSVGStringFromElement (Group elems, transList) = getSVGStringFromElements elems
@@ -90,3 +92,35 @@ printPoint::Point->String
 
 printPoint (PointC (x,y)) = 
     concat [show x, ",", show y, " "]
+
+
+
+getStringFromTransforms::[Transformation]->String
+
+getStringFromTransforms transList = 
+    let transtrs = map getStringFromTransform transList
+        initstr = " transform = \""
+        endstr = "\""
+    in concat [initstr, concat transtrs, endstr]
+
+
+getStringFromTransform::Transformation->String
+
+getStringFromTransform (TranslateC (x,y)) =
+    concat ["translate(", show x, ",", show y, ") "]
+
+getStringFromTransform (ScaleC (sx,sy)) = 
+    concat ["scale(", show sx, ",", show sy, ") "]
+
+getStringFromTransfrom (RotateC (angle, cx,cy)) = 
+    concat ["rotate(", show angle , ",", show cx, ",", show cy, ") "]
+
+getStringFromTransfrom (SkewXC angle) = 
+    concat ["skewX(", show angle, ") "]
+
+getStringFromTransfrom (SkewYC angle) = 
+    concat ["skewY(", show angle, ") "]
+
+getStringFromTransfrom (MatrixC (a,b,c,d,e,f)) = 
+    concat ["matrix(", show a, ",", show b, ",", show c, ",", show d, ",",
+             show e, ",", show f, ") "]
